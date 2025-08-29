ec3f18d0fa0f170074990cd18e4c40f859636895
Artur <49724524+oakartur@users.noreply.github.com>
2025-08-29 10:46:41 -0400

    fix: align db env variable

--- api/app/database.py
+++ api/app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DATABASE_URL = os.getenv("DATABASE_URL") or os.getenv("DATABASE_URLS", "")
 
 # garantir driver async
 if DATABASE_URL.startswith("postgresql://"):
